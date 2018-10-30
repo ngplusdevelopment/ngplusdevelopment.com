19eafa569f4047d632481287d4bfb54f7a0de64c
Rick Kuipers <rick@ngplusdevelopment.com>
2018-10-30 15:07:22 +0100

    Debugging Travis deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,8 +3,15 @@ set -e
 
 BASEDIR=$(dirname "$0")
 
+echo $BASEDIR
+
 eval "$(ssh-agent -s)"
+
+echo "Changing filemode"
 chmod 600 ${BASEDIR}/deploy_key
+
+echo "Adding SSH key"
 ssh-add ${BASEDIR}/deploy_key
 
+echo "Deploying"
 ssh ngplusdevelopment@ngplusdevelopment.com "cd /srv/http/ngplusdevelopment.com/current && git pull"
